ddbe5254c3fa5d3e91cd9796b44589cfd610ed03
Robert Pinl <robert.pinl@icloud.com>
2023-04-27 17:17:14 +1200

    Update client and server

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,8 +1,6 @@
-const API_URL = 'http://localhost:8000';
-
 async function httpGetPlanets() {
-  const response = await fetch(`${API_URL}/planets`);
-  return await response.json();
+  // TODO: Once API is ready.
+  // Load planets and return as JSON.
 }
 
 async function httpGetLaunches() {
